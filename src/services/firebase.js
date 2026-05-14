9adc0706139c706c8fb0ce201dff55d0994ca486
Mystify7777 <aryankumar.55566@gmail.com>
2026-05-14 18:21:01 +0530

    feat: enable IndexedDB persistence for Firestore and handle potential errors

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  enableIndexedDbPersistence,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +22,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+
+    if (err.code === "failed-precondition") {
+
+      console.error(
+        "Persistence failed: multiple tabs open"
+      );
+
+    } else if (
+      err.code === "unimplemented"
+    ) {
+
+      console.error(
+        "Persistence not supported"
+      );
+    }
+  });
